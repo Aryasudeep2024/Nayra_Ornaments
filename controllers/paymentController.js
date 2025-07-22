56cc114baedc74fbd9d300cbbcada81e9c8805ed
Aryasudeep2024 <arya32.2011@gmail.com>
2025-07-22 12:19:49 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,6 @@
 const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
-const client_domain = process.env.CLIENT_DOMAIN;
+
 
 exports.createCheckoutSession = async (req, res) => {
   try {
@@ -22,9 +22,9 @@ exports.createCheckoutSession = async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: `${client_domain}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.FRONTEND_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
 
-      cancel_url: `${client_domain}/payment-cancel`,
+      cancel_url: `${process.env.FRONTEND_URL}/payment-cancel`,
     });
 
     res.status(200).json({ success: true, sessionId: session.id });
